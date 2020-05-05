8cd21d5eec564b6e697bb74f90091c3bde5bb24e
Praval <praval@Palkeshs-MacBook-Air.local>
2020-05-05 15:37:34 +0530

    update 5 may challenge

---
+++ 5-may.js
@@ -0,0 +1,9 @@
+var firstUniqChar = function(s) {
+   for (var i = 0; i < s.length; i++) {
+    var c = s.charAt(i);
+    if (s.indexOf(c) == i && s.indexOf(c, i + 1) == -1) {
+      return i
+    }
+  }
+  return -1
+};
